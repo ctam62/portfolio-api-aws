166f756a598cc582c28ea8e33e47ed1d8a27ae39
claratam <claramtam@gmail.com>
2023-12-23 19:30:28 -0600

    init commit

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+module.exports = {
+    client: 'pg',
+    connection: {
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        charset: 'utf8',
+    },
+
+    onUpdateTimestampFunction: `
+    CREATE OR REPLACE FUNCTION on_update_timestamp()
+    RETURNS trigger AS $$
+    BEGIN
+      NEW.updated_at = now();
+      RETURN NEW;
+    END;
+    $$ language 'plpgsql';
+  `,
+
+    onUpdateTrigger: table => `
+    CREATE TRIGGER ${table}_updated_at
+    BEFORE UPDATE ON ${table}
+    FOR EACH ROW
+    EXECUTE PROCEDURE on_update_timestamp();
+  `
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(express.static("./public"));
+
+const projectRoutes = require("./routes/project-routes");
+app.use("/api/projects", projectRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
