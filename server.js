8681033433389836518f722e33db759489f8b050
claratam <claramtam@gmail.com>
2023-12-24 15:28:40 -0600

    fix: update cors origin

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
+app.use(cors({ origin: "*" }));
 app.use(express.static("./public/"));
 
 const projectRoutes = require("./routes/project-routes");
