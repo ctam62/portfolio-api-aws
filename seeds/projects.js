d9187131c94a46b8d0b046b0575ebe35e9ea5120
claratam <claramtam@gmail.com>
2023-12-24 18:31:57 -0600

    feat: update data seed file

--- seeds/projects.js
+++ seeds/projects.js
@@ -7,16 +7,13 @@ exports.seed = async function (knex) {
             "url": "",
             "client": "",
             "date": "2023 December",
-            "brief": "Create a solution to aid home cooks in everyday meal planning",
+            "brief": "Create a solution to aid home cooks in everyday meal planning.",
             "concept": "One-stop location for all your meal planning needs. Curate a comprehensive list of ingredients, discover, save, and automatically adjust recipes.",
             "deliverables": "Frontend and backend web development",
             "demographic": "Home cooks and food enthusiasts",
             "tech": ["Vite", "React.js", "Sass", "Express.js", "Node.js", "PostgreSQL"],
             "github": "https://github.com/ctam62/savoryscale",
-            "images": [
-                "savoryscale.jpg",
-                "savoryscale-responsive.jpg"
-            ]
+            "images": ["savoryscale.jpg", "savoryscale-responsive.jpg"]
         },
         {
             "title": "YouWatch",
@@ -24,16 +21,13 @@ exports.seed = async function (knex) {
             "url": "",
             "client": "Roku",
             "date": "2023 November",
-            "brief": "Create a solution to entice viewers to subscribe a new streaming platform",
+            "brief": "Create a solution to entice viewers to subscribe a new streaming platform.",
             "concept": "Empower users to effortlessly discover and explore personalized movies or TV shows.",
             "deliverables": "Frontend and backend web development",
             "demographic": "Movie and TV show fans, digital entertainment enthusiasts, and streaming providers",
             "tech": ["React.js", "Sass", "Express.js", "Node.js", "MySQL"],
             "github": "https://github.com/ctam62/industry-2b-server",
-            "images": [
-                "youwatch.jpg",
-                "youwatch-responsive.jpg"
-            ]
+            "images": ["youwatch.jpg", "youwatch-responsive.jpg"]
         },
         {
             "title": "iSpine",
@@ -41,19 +35,17 @@ exports.seed = async function (knex) {
             "url": "http://digitalimaginggroup.ca/ispine/",
             "client": "Digital Imaging Group of London",
             "date": "2018 April",
-            "brief": "Design a website for the iSpine program focused on project sharing and collaboration",
-            "concept": "Spinal medical research portfolio fostering collaboration",
+            "brief": "Design a website for the iSpine program focused on project sharing and collaboration.",
+            "concept": "Spinal medical research portfolio fostering collaboration.",
             "deliverables": "Frontend web design and development",
             "demographic": "Radiologists, medical imaging scientists, and biomedical engineers",
             "tech": ["HTML5", "CSS3", "Bootstrap", "JavaScript", "JQuery"],
             "github": "",
-            "images": [
-                "ispine.jpg"
-            ]
+            "images": ["ispine.jpg"]
         },
         {
             "title": "GIF Reveal Rumble",
-            "description": "A single player card matching game using GIF images from the GIPHY API. This was a hackathon project completed in a two person team.",
+            "description": "A single player card matching game using GIF images from the GIPHY API. This was a hackathon project completed in a two person software egineering team.",
             "url": "https://ctam62.github.io/gif-reveal-rumble/",
             "client": "",
             "date": "2023 October",
@@ -63,9 +55,7 @@ exports.seed = async function (knex) {
             "demographic": "GIF enthusiasts",
             "tech": ["HTML5", "Sass", "JavaScript"],
             "github": "https://github.com/ctam62/gif-reveal-rumble",
-            "images": [
-                "gifrevealrumble.jpg"
-            ]
+            "images": ["gifrevealrumble.jpg"]
         }
     ]);
 };
