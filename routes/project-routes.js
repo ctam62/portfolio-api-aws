7ee4442e610b966620ed2ab0fdc130dab516ee85
claratam <claramtam@gmail.com>
2023-12-26 16:10:24 -0600

    feat: add project-routes.js

---
+++ routes/project-routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const projectController = require("../controllers/project-controller");
+
+router
+    .route('/')
+    .get(projectController.getAllProjects);
+
+router
+    .route('/:title')
+    .get(projectController.getProjectByTitle);
+
+
+module.exports = router;
