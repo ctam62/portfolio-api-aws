1d5a62512da9080f719887213cfe2aedaed92545
claratam <claramtam@gmail.com>
2024-01-08 11:50:19 -0600

    feat: update aws secrets manager connection

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
-import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
+require("dotenv").config();
+const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
 
 const secret_name = "prod/portfolio/postgres";
 
@@ -6,28 +7,41 @@ const client = new SecretsManagerClient({
     region: "us-east-1",
 });
 
-let response;
+const getAWSSecrets = async () => {
+    try {
+        const response = await client.send(
+            new GetSecretValueCommand({
+                SecretId: secret_name,
+                VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+            }),
+        );
 
-try {
-    response = await client.send(
-        new GetSecretValueCommand({
-            SecretId: secret_name,
-            VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-        }),
-    );
-} catch (error) {
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    throw error;
-}
+        console.log("Secrets Manager call successful");
 
-const secret = response.SecretString;
+        const secret = JSON.parse(response.SecretString);
+        console.log(secret);
 
-require("dotenv").config();
+        const config = {
+            host: secret.POSTGRES_HOST,
+            database: secret.POSTGRES_DB,
+            user: secret.POSTGRES_USER,
+            password: secret.POSTGRES_PASSWORD,
+            charset: "utf8",
+        };
+
+        return config;
+    } catch (error) {
+        // For a list of exceptions thrown, see
+        // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        throw error;
+    }
+};
 
 module.exports = {
     client: "pg",
-    connection: process.env.POSTGRES_URL + "?sslmode=require",
+    connection: async () => {
+        return await getAWSSecrets();
+    },
 
     onUpdateTimestampFunction: `
     CREATE OR REPLACE FUNCTION on_update_timestamp()
